8ae4d113031c3f4dc9367cdc85b3803eeaa0ee32
pietroventurini <pietro97venturini@gmail.com>
2016-04-24 13:32:45 +0000

    Changed Key Path

--- getKanji.php
+++ getKanji.php
@@ -1,6 +1,6 @@
 <?php
     require_once '../unirest-php/src/Unirest.php';
-    $File = "APIKey.txt";
+    $File = "KEYS/APIKey.txt";
     $MashapeKey = file_get_contents($File);
     $keyword = $_GET['keyword'];
     $response = Unirest\Request::get("https://kanjialive-api.p.mashape.com/api/public/search/" . strtolower($keyword),
--- getKanjiDetails.php
+++ getKanjiDetails.php
@@ -1,6 +1,6 @@
 <?php
     require_once '../unirest-php/src/Unirest.php';
-    $File = "APIKey.txt";
+    $File = "KEYS/APIKey.txt";
     $MashapeKey = file_get_contents($File);
     $character = urlencode($_GET['character']);
     $response = Unirest\Request::get("https://kanjialive-api.p.mashape.com/api/public/kanji/" . $character,
