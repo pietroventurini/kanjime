50e032df11b05a93e4bcf0ff8b24056676a56f7e
pietroventurini <pietro97venturini@gmail.com>
2016-05-06 15:14:42 +0000

    encoded URL

--- getKanji.php
+++ getKanji.php
@@ -2,7 +2,7 @@
     require_once '../unirest-php/src/Unirest.php';
     $File = "KEYS/APIKey.txt";
     $MashapeKey = file_get_contents($File);
-    $keyword = $_GET['keyword'];
+    $keyword = urlencode($_GET['keyword']);
     $response = Unirest\Request::get("https://kanjialive-api.p.mashape.com/api/public/search/" . strtolower($keyword),
       array(
         "X-Mashape-Key" => $MashapeKey,
